eed7162e55affa3fbfd4e0801dc8048249205e25
vhalitsyn <viktor.halitsyn@outlook.com>
2024-05-20 23:26:07 -0700

    adding removal and slack notification. moving to cron docker

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,15 @@ LABEL maintainer="rdev02 <rdev02@outlook.com>"
 
 RUN pip --no-cache-dir install slack-cleaner2
 
+ENV SLACK_TOKEN=MISSING
+ENV SLACK_CHANNEL=MISSING
+
 WORKDIR /opt/work
 ADD remove_slack_msgs.py remove_slack_msgs.py
 
-ENV SLACK_TOKEN=MISSING
+# Add the cron job
+RUN echo "0 0 * * * python /opt/work/remove_slack_msgs.py" > /etc/crontabs/root
+# Redirect cron logs to Docker logs
+RUN ln -sf /dev/stdout /var/log/cron.log
 
-CMD ["python", "/opt/work/remove_slack_msgs.py"]
+CMD ["crond", "-f", "-l", "8"]
--- remove_slack_msgs.py
+++ remove_slack_msgs.py
@@ -1,45 +1,96 @@
 from slack_cleaner2 import *
-from datetime import datetime
-from collections import defaultdict
 import os
+import requests
+import json
 
-s = SlackCleaner(os.environ["SLACK_TOKEN"])
-stats = {};
 
-for u in s.users:
-    stats[u.id] = {
-      'name': u.name,
-      'messages': defaultdict(int),
-      'files': 0
+def post_message_to_slack(token, channel, text):
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    payload = {
+        "channel": channel,
+        "text": text
     }
 
-for c in s.ims:
-  for msg in c.msgs(before=a_while_ago(months=1)):
-    stats[msg.user.id]['messages'][datetime.fromtimestamp(msg.ts).year] += 1
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
 
-for c in s.mpim:
-  for msg in c.msgs(before=a_while_ago(months=1)):
-    stats[msg.user.id]['messages'][datetime.fromtimestamp(msg.ts).year] += 1
+    if response.status_code == 200:
+        print("Message posted successfully!")
+        return response.json()
+    else:
+        print(f"Failed to post message. Status code: {response.status_code}")
+        print(f"Response: {response.text}")
+        return None
 
-for channel in s.conversations:
-  for msg in channel.msgs(before=a_while_ago(months=1), with_replies=True):
-    if not msg.user_id:
-      continue #sys messages
-    stats[msg.user_id]['messages'][datetime.fromtimestamp(msg.ts).year] += 1
-'''
-for file in s.files(before=a_while_ago(months=1)):
-  if not file.user_id:
-    continue
-  stats[file.user.id]['files'] += 1
-'''
+def clean_slack(timeBefore):
+  s = SlackCleaner(os.environ["SLACK_TOKEN"])
+  stats = {};
+
+  for u in s.users:
+      stats[u.id] = {
+        'name': u.name,
+        'removedMsg': 0,
+        'failedMsg': 0,
+        'removedFiles': 0,
+        'failedFiles': 0
+      }
+
+  for c in s.ims:
+    for msg in c.msgs(before=timeBefore, with_replies=True):
+      if msg.delete() == None:
+        stats[msg.user.id]['removedMsg'] += 1
+      else:
+        stats[msg.user.id]['failedMsg'] += 1
+
+  for c in s.mpim:
+    for msg in c.msgs(before=timeBefore, with_replies=True):
+      if msg.delete() == None:
+        stats[msg.user.id]['removedMsg'] += 1
+      else:
+        stats[msg.user.id]['failedMsg'] += 1
+
+  for channel in s.conversations:
+    for msg in channel.msgs(before=timeBefore, with_replies=True):
+      if not msg.user_id:
+        continue #sys messages
+      if msg.delete() == None:
+        stats[msg.user.id]['removedMsg'] += 1
+      else:
+        stats[msg.user.id]['failedMsg'] += 1
+  
+  for file in s.files(before=a_while_ago(months=1)):
+    if not file.user.id:
+      continue
+    if file.delete() == None:
+      stats[file.user.id]['removedFiles'] += 1
+    else: 
+      stats[file.user.id]['failedFiles'] += 1
+  
+  return stats
+
+# Your OAuth token
+oauth_token = os.environ["SLACK_TOKEN"]
+report_channel_name = "#" + os.environ["SLACK_CHANNEL"]
+
+stats = clean_slack(a_while_ago(months=1))
+
+userStatsStr = 'removed ```';
 for uId in stats.keys():
-  msgStr = ''
-  totalmsgs = 0
-  for yr in stats[uId]['messages']:
-    totalmsgs += stats[uId]['messages'][yr]
-    msgStr += '; ' + str(yr) + ': ' + str(stats[uId]['messages'][yr])
+  uname = stats[uId]['name']
+  totalmsgs = stats[uId]['removedMsg']
+  totalFailedmsgs = stats[uId]['failedMsg']
+  if totalmsgs == 0 and totalFailedmsgs == 0:
+     print(f'skipping {uname}')
+     continue
 
-  msgStr = 'Total: ' + str(totalmsgs) + ' messages. ' + msgStr # + str(stats[uId]['files']) + ' files; '  + msgStr
-  print(stats[uId]['name'] + ': ' + msgStr) 
-'''
-'''
+  totalFiles = stats[uId]['removedFiles']
+  totalFailedFiles = stats[uId]['failedFiles']
+  
+  userStatsStr += f"{uname}: {totalmsgs} msgs / {} failed; {totalFiles} files / {totalFailedFiles} failed\n" 
+userStatsStr += "```"
+
+print(userStatsStr)
+post_message_to_slack(oauth_token, report_channel_name, userStatsStr)
